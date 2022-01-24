4a932bc966452959ff24ffd79116cb3041544229
rahmatrafli1 <rahmatrafli1@gmail.com>
2022-01-24 19:00:38 +0700

    second commit

--- application/views/input_barang.php
+++ application/views/input_barang.php
@@ -5,12 +5,15 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 	<title><?php echo $judul; ?></title>
 </head>
 
 <body>
 	<?php echo form_open('barang/input_simpan'); ?>
-	<table>
+	<table class="ms-2 mt-2">
 		<tr>
 			<td>KODE BARANG</td>
 			<td><?php echo form_input('kode_barang', '', ['placeholder' => 'Kode Barang']); ?></td>
@@ -25,12 +28,13 @@
 		</tr>
 		<tr>
 			<td colspan="2">
-				<?php echo form_submit('SUBMIT', 'SIMPAN DATA'); ?>
-				<?php echo anchor('', 'Kembali'); ?>
+				<?php echo form_submit('SUBMIT', 'SIMPAN DATA', array('class' => 'btn btn-primary')); ?>
+				<?php echo anchor('', 'Kembali', array('class' => 'btn btn-secondary')); ?>
 			</td>
 		</tr>
 	</table>
 
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- application/views/list_barang.php
+++ application/views/list_barang.php
@@ -5,14 +5,17 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 	<title><?php echo $judul; ?></title>
 </head>
 
 <body>
-	<?php echo anchor('barang/input', 'INPUT DATA BARANG'); ?>
+	<?php echo anchor('barang/input', 'INPUT DATA BARANG', array('class' => 'btn btn-success mt-3 ms-3')); ?>
 	<hr>
 
-	<table border="1">
+	<table class="table">
 		<tr>
 			<th>KODE BARANG</th>
 			<th>NAMA BARANG</th>
@@ -25,8 +28,8 @@
 				<td>$b->kode_barang</td>
 				<td>$b->nama_barang</td>
 				<td>$b->harga</td>
-				<td>" . anchor('barang/edit/' . $b->kode_barang, 'EDIT') . "</td>
-				<td>" . anchor('barang/delete/' . $b->kode_barang, 'HAPUS', array('onclick' => "return ConfirmDialog();")) . "</td>
+				<td>" . anchor('barang/edit/' . $b->kode_barang, 'EDIT', array('class' => 'btn btn-warning')) . "</td>
+				<td>" . anchor('barang/delete/' . $b->kode_barang, 'HAPUS', array('onclick' => "return ConfirmDialog();", 'class' => 'btn btn-danger')) . "</td>
 			</tr>";
 		}
 		?>
@@ -42,7 +45,7 @@
 			}
 		}
 	</script>
-
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
